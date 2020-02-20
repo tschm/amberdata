c81878d8a8c3fe3fd7de9dd1da6edf605c6f0375
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2020-02-20 11:12:41 +0100

    util for pandas

---
+++ pyamber/util.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def dict2series(ts):
+    return pd.Series({pd.Timestamp(1e6*int(x["timestamp"])): x["price"] for x in ts})
+
+
+def payload2frame(payload):
+    return pd.DataFrame({name: dict2series(ts) for name, ts in payload.items()})
+
--- scripts/health.py
+++ scripts/health.py
@@ -1,11 +1,22 @@
+import pandas as pd
+
 from pyamber.request import AmberRequest
+from pyamber.util import dict2series, payload2frame
 
 if __name__ == '__main__':
-    a = AmberRequest()
-    response = a.get(url="https://web3api.io/health")
-    print(response)
+    payload = AmberRequest()
 
     # url = "https://web3api.io/api/v2/market/prices/eth_usd/latest"
-    response = a.get(url="https://web3api.io/api/v2/market/prices/eth_usd/historical")
+    response = payload.get(url="https://web3api.io/api/v2/market/prices/eth_usd/historical")
     print(response.text)
+
+    payload = response.json()["payload"]
+    print(payload2frame(payload))
+
+
+
+
+
+
+
 
