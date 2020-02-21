61dc2f237dc15efb04686f5f868972e729b56bda
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2020-02-21 11:27:13 +0100

    util for pandas

--- scripts/health.py
+++ scripts/health.py
@@ -1,44 +1,58 @@
 from pyamber.request import AmberRequest
-from pyamber.util import  payload2frame
+from pyamber.util import payload2frame
+from enum import Enum
 
 import pandas as pd
+pd.Timestamp.value_in_milliseconds = property(lambda self: int(self.value*1e-6))
+
+
+class TimeFormat(Enum):
+    MILLISECONDS = "milliseconds"
+    MS = "ms"
+    ISO = "iso"
+    ISO8611 = "iso8611"
+
+
+class TimeInterval(Enum):
+    DAYS = "days"
+    HOURS = "hours"
+    MINUTES = "minutes"
+
+
+def historical(pair, timeInterval=None, startDate=None, endDate=None, timeFormat=None):
+    pd.Timestamp.value_in_milliseconds = property(lambda self: int(self.value*1e-6))
+
+    startDate = (startDate or pd.Timestamp("today")).value_in_milliseconds
+    endDate = (endDate or pd.Timestamp("today")).value_in_milliseconds
+
+    gap = endDate - startDate
+    timeInterval = timeInterval or TimeInterval.HOURS
+    timeFormat = timeFormat or TimeFormat.MILLISECONDS
+
+    url="https://web3api.io/api/v2/market/prices/{pair}/historical".format(pair=pair)
+    params = {"timeInterval": timeInterval.value, "startDate": startDate, "endDate": endDate, "timeFormat": timeFormat.value}
+
+
+    request = AmberRequest()
+    response = request.get(url=url, params=params)
+
+    request = response.json()["payload"]
+    return payload2frame(request)
+
 
 if __name__ == '__main__':
     def f(t):
-        return int(t.value*1e-6)
+        return int(t.value * 1e-6)
 
-    def f_inv(i):
-        return pd.Timestamp(1e6*int(i))
+    x = pd.Timestamp("today")
+    pd.Timestamp.value_in_milliseconds = property(lambda self: int(self.value * 1e-6))
+    print(x.value_in_milliseconds)
 
-    # start at the beginning of Feb 2020
-    startDate = f(pd.Timestamp("2020-02-01"))
-    assert startDate == 1580515200000
+    pair = "eth_usd"
 
-    # add the maximal gap to the startDate
-    maxGap = 2678400000 - 1000
-    endDate = startDate + maxGap
+    #print(historical(pair=pair, startDate=pd.Timestamp("today"), endDate=pd.Timestamp("today") + pd.DateOffset(days=1)))
 
-    print(endDate)
-    print(f_inv(endDate))
+    #print(response.text)
 
-    request = AmberRequest()
-
-    # url = "https://web3api.io/api/v2/market/prices/eth_usd/latest"
-    response = request.get(url="https://web3api.io/api/v2/market/prices/eth_usd/historical",
-                           params={"timeInterval":"hours", "startDate": str(startDate), "endDate": str(endDate)})
-
-
-
-    print(response.text)
-
-    request = response.json()["payload"]
-    print(payload2frame(request))
-
-
-
-
-
-
-
-
-
+    #request = response.json()["payload"]
+    #print(payload2frame(request))
