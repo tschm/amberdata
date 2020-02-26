9fa2a35e1f2ba460668302cc80367e52a172938e
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2020-02-26 14:08:34 +0100

    check dist

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 from pyamber import __version__ as version
 
 # read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#from os import path
+#this_directory = path.abspath(path.dirname(__file__))
+
+with open('README.md') as f:
     long_description = f.read()
 
 setup(
