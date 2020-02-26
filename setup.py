1894391091bc349c7f891063100261f83d076c1d
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2020-02-26 14:04:00 +0100

    setup with README

--- setup.py
+++ setup.py
@@ -3,8 +3,16 @@
 from setuptools import setup, find_packages
 from pyamber import __version__ as version
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='pyamber',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version=version,
     packages=find_packages(include=["pyamber*"]),
     author='Thomas Schmelzer',
