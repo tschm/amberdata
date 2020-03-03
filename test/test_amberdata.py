c1763b319e19e1c8309c555ffa0e89065da22f3d
Thomas Schmelzer <thomas.schmlezer@gmail.com>
2020-03-03 21:43:04 +0100

    towards ohlcv history

--- test/test_amberdata.py
+++ test/test_amberdata.py
@@ -21,42 +21,6 @@ def test_history():
         AmberRequest(key="a").prices.history(pair="eth_usd")
 
 
-
-# @patch.object(AmberRequest, 'get')
-# def test_history_ohlcv(get_ohlcv):
-#     for exchange, data in AmberRequest(key="a").ohlcv.history(pair="eth_usd", start_date=pd.Timestamp("2020-01-15"), end_date=pd.Timestamp("2020-01-17"), time_interval=TimeInterval.HOURS, exchange="bitfinex"):
-#         pass
-#
-#     get_ohlcv.assert_called_once_with(params={'timeInterval': 'hours', 'startDate': 1579046400000, 'endDate': 1579219200000, 'timeFormat': 'milliseconds', 'exchange': "bitfinex"},
-#                                       url='https://web3api.io/api/v2/market/ohlcv/eth_usd/historical')
-#
-#
-# @patch.object(AmberRequest, 'get')
-# def test_history_bid_ask(get_bid_ask):
-#     for exchange, data in AmberRequest(key="a").bid_ask.history(pair="eth_usd", start_date=pd.Timestamp("2020-01-12"), end_date=pd.Timestamp("2020-01-13"), exchange="bitfinex"):
-#         pass
-#
-#     get_bid_ask.assert_called_once_with(params={'endDate': 1578873600000, 'startDate': 1578787200000, 'exchange': "bitfinex"},
-#                                         url='https://web3api.io/api/v2/market/tickers/eth_usd/historical')
-#
-#
-# def test_history_bid_ask_2():
-#     with requests_mock.Mocker() as m:
-#         m.get("https://web3api.io/api/v2/market/tickers/eth_usd/historical",
-#                           json={"status": 200,
-#                                 "payload": {"metadata":
-#                                                 {"columns": ["timestamp", "bid", "ask", "mid", "last"]},
-#                                             "data":
-#                                                 {"bitfinex": [[1578873600000, 100, 102, 101, 101.5],
-#                                                               [1578787200000, 101, 102, 101.5, 101.2]]}
-#                                             }})
-#
-#         d = {exchange: data for exchange, data in AmberRequest(key="a").bid_ask.history(pair="eth_usd", start_date=pd.Timestamp("2020-01-12"), end_date=pd.Timestamp("2020-01-13"), exchange="bitfinex")}
-#         assert set(d.keys()) == {"bitfinex"}
-#         assert list(d["bitfinex"].index) == [pd.Timestamp("2020-01-13"), pd.Timestamp("2020-01-12")]
-#         assert d["bitfinex"]["rel. spread"][pd.Timestamp("2020-01-13")] == pytest.approx(2.0/101.0, 1e-10)
-
-
 def test_prices_latest():
     with requests_mock.Mocker() as m:
         m.get("https://web3api.io/api/v2/market/prices/eth_usd/latest", json=read_json("prices_latest.json"))
@@ -102,6 +66,16 @@ def test_bidask_history():
             assert exchange == "bitfinex"
             pdt.assert_frame_equal(data, read_pd("bidask_history.csv", index_col=0, parse_dates=True))
 
+
+def test_ohlcv_history():
+    with requests_mock.Mocker() as m:
+        m.get("https://web3api.io/api/v2/market/ohlcv/eth_usd/historical?timeInterval=days&startDate=1577836800000", json=read_json("ohlcv_history1.json"))
+        m.get("https://web3api.io/api/v2/market/ohlcv/eth_usd/historical?timeInterval=days&startDate=1579564800000", json=read_json("ohlcv_history2.json"))
+        m.get("https://web3api.io/api/v2/market/ohlcv/eth_usd/historical?timeInterval=days&startDate=1581292800000", json=read_json("ohlcv_history3.json"))
+
+        for exchange, data in AmberRequest(key="a").ohlcv.history(pair="eth_usd", exchange="bitfinex", start_date=pd.Timestamp("2020-01-01"), end_date=pd.Timestamp("2020-02-20"), time_interval=TimeInterval.DAYS):
+            assert exchange == "bitfinex"
+            pdt.assert_frame_equal(data, read_pd("ohlcv_history.csv", index_col=0, parse_dates=True))
 
 
 def test_prices_history():
